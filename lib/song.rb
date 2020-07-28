1de93c963cb61f45651db39cf467c6c3c710b1f7
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-28 18:26:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
     @@all.sort_by {|song| song.name}
   end
 
-  def self.new_from_filename(filename)
+  def self.new_from_filename
     
     rows = filename.split(" - ")
     artist_name = rows[0]
